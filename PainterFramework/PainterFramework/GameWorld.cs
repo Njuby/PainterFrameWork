cffe5c60b8b53528ae22ba388bf42680690e9adf
DESKTOP-IOTBAVS\David Elout <dedavid.elout@gmail.com>
2017-03-13 13:53:54 +0100

    Barrel can rotate perfetly and change color

--- PainterFramework/PainterFramework/GameWorld.cs
+++ PainterFramework/PainterFramework/GameWorld.cs
@@ -3,18 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
 namespace PainterFramework
 {
     class GameWorld : GameObjectList
     {
         private SpriteGameObject backround = null;
+        private RotatableSpriteObject cannonBarrel = null;
+        private ThreeColorGameObject cannonColor = null;
 
         public GameWorld()
         {
             backround = new SpriteGameObject("spr_background");
+            cannonBarrel = new RotatableSpriteObject("spr_cannon_barrel");
+            cannonBarrel.Position = new Vector2(74, 404);
+            cannonBarrel.Origin = new Vector2(34, 34);
+
+            cannonColor = new ThreeColorGameObject("spr_cannon_red", "spr_cannon_green", "spr_cannon_blue");
+            cannonColor.Position = new Vector2(58, 388);
+            
+
             this.Add(backround);
+            this.Add(cannonBarrel);
+            this.Add(cannonColor);
         }
 
-        
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+
+            if (inputHelper.KeyPressed(Keys.R))
+                cannonColor.Color = Color.Red;
+            else if (inputHelper.KeyPressed(Keys.G))
+                cannonColor.Color = Color.Green;
+            else if (inputHelper.KeyPressed(Keys.B))
+                cannonColor.Color = Color.Blue;
+
+            double opposite = inputHelper.MousePosition.Y - cannonBarrel.GlobalPosition.Y;
+            double adjectent = inputHelper.MousePosition.X - cannonBarrel.GlobalPosition.X;
+            cannonBarrel.Angle = (float)Math.Atan2(opposite, adjectent);
+        }
+
+
     }
 }
---
+++ PainterFramework/PainterFramework/RotatableSpriteObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PainterFramework
+{
+    class RotatableSpriteObject : SpriteGameObject
+    {
+        protected float angle;
+
+        public RotatableSpriteObject(string assetname, int layer = 0, string id = "", int sheetIndex = 0)
+            : base(assetname, layer, id, sheetIndex)
+        {
+            angle = -0.5f;
+        }
+
+        public float Angle
+        {
+            get { return angle; }
+            set
+            { angle = value; }
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            if (!visible || sprite == null)
+                return;
+
+            spriteBatch.Draw(sprite.Sprite, this.GlobalPosition, null, Color.White, angle, this.Origin, 1.0f, SpriteEffects.None, 0);
+        }
+    }
+}
